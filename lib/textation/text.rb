1a7bfe9eb5415ab81cdd642bdb850a77417325f5
iva mudronja <iva.pilizota@gmail.com>
2020-12-02 20:01:48 +0100

    analyze text

--- lib/textation/text.rb
+++ lib/textation/text.rb
@@ -6,15 +6,17 @@ require "textation/version"
 
     def analyze_file(file_name)
       text = File.open(file_name, 'r').read
-      stop_words = File.open('data/stop_words.txt', 'r').read.split(/\b/)
-      useful_words = text.downcase.split(/\b/).delete_if { |word| stop_words.include?(word) || word.match?(/\W/) }.length
+      #stop_words = File.open('data/stop_words.txt', 'r').read.split(/\b/)
+      #useful_words = text.downcase.split(/\b/).delete_if { |word| stop_words.include?(word) || word.match?(/\W/) }.length
       result = analyze(text)
-      result[:percentage_useful_words] = (useful_words.to_f / result[:word_count]).round(1) * 100
+      #result[:percentage_useful_words] = (useful_words.to_f / result[:word_count]).round(1) * 100
       result
     end
 
     def analyze(text)
       result = {}
+      stop_words = File.open('data/stop_words.txt', 'r').read.split(/\b/)
+      useful_words = text.downcase.split(/\b/).delete_if { |word| stop_words.include?(word) || word.match?(/\W/) }.length
       result[:character_count] = text.length
       result[:character_count_excluding_spaces] = text.gsub(/\s/, '').length
       result[:line_count] = text.split(/\r\n/).length
@@ -23,6 +25,7 @@ require "textation/version"
       result[:paragraph_count] = text.split(/\r\n\r\n/).length
       result[:average_words_per_sentence] = (result[:word_count].to_f / result[:sentence_count]).round(1)
       result[:average_sentences_per_paragraph] = (result[:sentence_count].to_f / result[:paragraph_count]).round(1)
+      result[:percentage_useful_words] = (useful_words.to_f / result[:word_count]).round(1) * 100
       result
     end
 
