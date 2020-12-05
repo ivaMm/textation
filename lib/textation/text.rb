aaaa03fab757c7d6182899a306054203ea6f4d12
iva mudronja <iva.pilizota@gmail.com>
2020-12-05 20:47:27 +0100

    improved

--- lib/textation/text.rb
+++ lib/textation/text.rb
@@ -39,60 +39,60 @@ require "textation/version"
           .delete_if { |w| stop_words.include?(w) }
           .select { |w| w.length >= 1 }
           .uniq
-      end
+    end
 
-      def top_words_stop(text, num = 3)
-        text = useful_words(text)
-        top_words(text, num)
-      end
+    def top_words_no_stop_words(text, num = 3)
+      text = useful_words(text)
+      top_words(text, num)
+    end
 
-      def top_words_no(text, num = 3)
-        text = no_emojis(text.downcase.split(' '))
-        top_words(text, num)
-      end
+    def top_words_all(text, num = 3)
+      text = text.downcase.split(/\b/)
+      top_words(text, num)
+    end
 
-      def top_words(text, num)
-        text.group_by(&:itself)
-            .transform_values(&:count)
-            .sort_by { |_k, v| -v }
-            .first(num)
-            .map(&:first).join(', ')
-      end
+    def top_words(text, num)
+      text.group_by(&:itself)
+          .transform_values(&:count)
+          .sort_by { |_k, v| -v }
+          .first(num)
+          .map(&:first).join(', ')
+    end
 
-      def occurences_of_words(text)
-        text.downcase
-            .split(/[^[[:alpha:]]]/)
-            .delete_if(&:empty?)
-            .group_by(&:itself)
-            .transform_values(&:count)
-            .sort_by { |_k, v| -v }
-            .to_h
-      end
+    def occurences_of_words(text)
+      text.downcase
+          .split(/[^[[:alpha:]]]/)
+          .delete_if(&:empty?)
+          .group_by(&:itself)
+          .transform_values(&:count)
+          .sort_by { |_k, v| -v }
+          .to_h
+    end
 
-      def occurence_of_word(text, word)
-        occurences_of_words(text)[word.downcase]
-      end
+    def occurence_of_word(text, word)
+      occurences_of_words(text)[word.downcase]
+    end
 
-      def percentage_of_words(text)
-        occurences = occurences_of_words(text)
-        len = text.split(/[^[[:alpha:]]]/).length
-        occurences.transform_values { |v| (v.to_f / len).round(1) * 100 }
-      end
+    def percentage_of_words(text)
+      occurences = occurences_of_words(text)
+      len = text.split(/[^[[:alpha:]]]/).length
+      occurences.transform_values { |v| (v.to_f / len).round(1) * 100 }
+    end
 
-      def percentage_of_word(text, word)
-        percentage_of_words(text)[word.downcase]
-      end
+    def percentage_of_word(text, word)
+      percentage_of_words(text)[word.downcase]
+    end
 
-      def syllables_per_lines(text)
-        text.downcase.split(/\n/).map do |line|
-          line.split(/\W+/).map do |word|
-            if word.split(/[^aeiouy]+/).delete_if(&:empty?).length > 1
-              word.gsub(/e$|es$|ed$/, "").split(/[^aeiouy]+/).delete_if(&:empty?).length
-            else
-              word.split(/[^aeiouy]+/).delete_if(&:empty?).length
-            end
+    def syllables_per_lines(text)
+      text.downcase.split(/\n/).map do |line|
+        line.split(/\W+/).map do |word|
+          if word.split(/[^aeiouy]+/).delete_if(&:empty?).length > 1
+            word.gsub(/e$|e{1}[a-z&&[^aeiou]*]$/, "").split(/[^aeiouy]+/).delete_if(&:empty?).length
+          else
+            word.split(/[^aeiouy]+/).delete_if(&:empty?).length
           end
-        end.map(&:sum)
-      end
+        end
+      end.map(&:sum)
+    end
   end
 #end
