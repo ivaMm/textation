e37c4e23e186e3b610d72e716374299916dfd353
iva mudronja <iva.pilizota@gmail.com>
2020-12-05 18:53:13 +0100

    improved

--- lib/textation/text.rb
+++ lib/textation/text.rb
@@ -6,37 +6,82 @@ require "textation/version"
 
     def analyze_file(file_name)
       text = File.open(file_name, 'r').read
-      #stop_words = File.open('data/stop_words.txt', 'r').read.split(/\b/)
-      #useful_words = text.downcase.split(/\b/).delete_if { |word| stop_words.include?(word) || word.match?(/\W/) }.length
-      result = analyze(text)
-      #result[:percentage_useful_words] = (useful_words.to_f / result[:word_count]).round(1) * 100
-      result
+      analyze(text)
     end
 
     def analyze(text)
       result = {}
-      stop_words = File.open('data/stop_words.txt', 'r').read.split(/\b/)
-      useful_words = text.downcase.split(/\b/).delete_if { |word| stop_words.include?(word) || word.match?(/\W/) }.length
       result[:character_count] = text.length
-      result[:character_count_excluding_spaces] = text.gsub(/\s/, '').length
-      result[:line_count] = text.split(/\r\n/).length
-      result[:word_count] = text.split(' ').length
-      result[:sentence_count] = text.split(/\w{2,}\.\s?\r?\n?[[:upper:]]|\?\!|\?|\!/).length
-      result[:paragraph_count] = text.split(/\r\n\r\n/).length
+      result[:character_count_excluding_spaces] = text.gsub(/\s/, "").length
+      result[:letter_count] = text.gsub(/[^[[:alpha:]]]/, "").length
+      result[:line_count] = text.split(/\n/).length
+      result[:word_count] = text.split(/[^[[:alpha:]]]/).length
+      result[:sentence_count] = text.split(/\.{1,3}\s?\r?\n?|\?!|\?|!/).length
+      result[:paragraph] = text.split(/\n\n/)
+      result[:paragraph_count] = result[:paragraph].length
+      result[:lines_per_paragraph] = result[:paragraph].map { |p| p.split(/\n/).length }
+      result[:syllables_per_lines] = syllables_per_lines(text)
       result[:average_words_per_sentence] = (result[:word_count].to_f / result[:sentence_count]).round(1)
       result[:average_sentences_per_paragraph] = (result[:sentence_count].to_f / result[:paragraph_count]).round(1)
-      result[:percentage_useful_words] = (useful_words.to_f / result[:word_count]).round(1) * 100
+      result[:useful_words] = useful_words(text)
+      result[:percentage_of_useful_words] = (useful_words(text).length.to_f / result[:word_count]).round(1) * 100
+      result[:unique_words] = text.downcase.split(/[^[[:alpha:]]]/).select { |w| w.length >= 1 }.uniq
+      result[:percentage_of_unique_words] = (result[:unique_words].length.to_f / result[:word_count]).round(1) * 100
+      result[:occurences_of_words] = occurences_of_words(text)
       result
     end
 
-    def top_words(text, num)
+    def useful_words(text)
+      stop_words = File.open('data/stop_words.txt', 'r').read.split(/\b/)
       text.downcase
-          .scan(/\w+[\w']*/)
-          .group_by(&:itself)
-          .transform_values(&:count)
-          .sort_by { |k, v| -v }
-          .first(num)
-          .map(&:first).join(', ')
-    end
+          .split(/[^[[:alpha:]]]/)
+          .delete_if { |w| stop_words.include?(w) }
+          .select { |w| w.length >= 1 }
+          .uniq
+      end
+
+      def top_words_stop(text, num = 3)
+        text = useful_words(text)
+        top_words(text, num)
+      end
+
+      def top_words_no(text, num = 3)
+        text = no_emojis(text.downcase.split(' '))
+        top_words(text, num)
+      end
+
+      def top_words(text, num)
+        text.group_by(&:itself)
+            .transform_values(&:count)
+            .sort_by { |_k, v| -v }
+            .first(num)
+            .map(&:first).join(', ')
+      end
+
+      def occurences_of_words(text)
+        text.downcase
+            .split(/[^[[:alpha:]]]/)
+            .delete_if(&:empty?)
+            .group_by(&:itself)
+            .transform_values(&:count)
+            .sort_by { |_k, v| -v }
+            .to_h
+      end
+
+      def occurence_of_word(text, word)
+        occurences_of_words(text)[word.downcase]
+      end
+
+      def syllables_per_lines(text)
+        text.downcase.split(/\n/).map do |line|
+          line.split(/\W+/).map do |word|
+            if word.split(/[^aeiouy]+/).delete_if(&:empty?).length > 1
+              word.gsub(/e$|es$|ed$/, "").split(/[^aeiouy]+/).delete_if(&:empty?).length
+            else
+              word.split(/[^aeiouy]+/).delete_if(&:empty?).length
+            end
+          end
+        end.map(&:sum)
+      end
   end
 #end
--- spec/textation_spec.rb
+++ spec/textation_spec.rb
@@ -2,16 +2,37 @@ require 'textation/text'
 
 describe Text do
   tx = Text.new
-  it 'text analyze' do
-    expect(tx.analyze('I love Ruby!')).to be_a Hash
-  end
+  result = {
+      character_count: 31,
+      character_count_excluding_spaces: 25,
+      letter_count: 23,
+      line_count: 1,
+      word_count: 8,
+      sentence_count: 2,
+      paragraph: ['I love Ruby! And Ruby loves me!'],
+      paragraph_count: 1,
+      lines_per_paragraph: [1],
+      syllables_per_lines: [9],
+      average_words_per_sentence: 4.0,
+      average_sentences_per_paragraph: 2.0,
+      useful_words:  ["love", "ruby", "loves"],
+      percentage_of_useful_words: 40.0,
+      unique_words:  ["i", "love", "ruby", "and", "loves", "me"],
+      percentage_of_unique_words: 80.0,
+      occurences_of_words: {"ruby"=>2, "i"=>1, "love"=>1, "and"=>1, "loves"=>1, "me"=>1}
+  }
 
-  it 'analyze file' do
+  it 'text file analyze' do
     expect(tx.analyze_file('data/test_source_text.txt')).to be_a Hash
   end
 
+  it 'text analyze' do
+    expect(tx.analyze('I love Ruby!')).to be_a Hash
+    expect(tx.analyze('I love Ruby! And Ruby loves me!')).to eq(result)
+  end
+
   it 'top words' do
-    expect(tx.top_words('I love Ruby! And Ruby loves me!', 1)).to eq('ruby')
+    expect(tx.occurence_of_word('I love Ruby! And Ruby loves me!', 'ruby')).to eq(2)
   end
 end
 
